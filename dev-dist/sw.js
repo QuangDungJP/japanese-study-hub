91274fd331b8ae29f69a762336fdc6497b21375e
Stephen South <stephensouth1307@gmail.com>
2026-09-15 17:51:41 +0700

    Update sw.js

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.vbjh2og22pg"
+    "revision": "0.jdm16eklji8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
